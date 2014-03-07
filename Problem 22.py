0ba61d9be4316479d7381ceab3e62e242809da0f
Stevenlysc <stevenlysc@gmail.com>
2014-03-07 15:22:33 -0600

    problem 22 modified

--- Problem 22.py
+++ Problem 22.py
@@ -7,11 +7,26 @@ For example, when the list is sorted into alphabetical order, COLIN, which is wo
 What is the total of all the name scores in the file?
 '''
 
-from Utilities import amicable_number
+import urllib
+import re
+
+def order_worth(name):
+    worth = 0
+    for i in name:
+        worth += ord(i) - 64
+    return worth
 
 def main():
-    amicableDict = amicable_number(10000)
-    print sum(amicableDict.keys()) + sum(amicableDict.values())
+    names = urllib.urlopen('http://projecteuler.net/project/names.txt').read()
+    r = re.compile(r'"(.+?)"')
+    result = re.findall(r, names)
+    result = sorted(result)
+    
+    name_score = 0
+    for i in xrange(len(result)):
+        name_score += (i + 1) * order_worth(result[i])
+    
+    print name_score
 
 if __name__ == '__main__':
     main()
